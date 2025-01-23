49c52ca576dbe062a77cd4e2a76d839db8511adc
doorungzi <doorungzi21@gmail.com>
2025-01-23 12:17:09 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -15,19 +15,3 @@ $(function(){
         $(this).siblings().children().children(".btn_more").slideDown()
     });
 });
-
-$(document).ready(function () {
-    var typingBool = false;
-    var typingIdx = 0;
-    var liIndex = 0;
-    var liLength = $(".typing-txt>ul>li").length;
-  
-    // 타이핑될 텍스트를 가져온다
-    var typingTxt = $(".typing-txt>ul>li").eq(liIndex).text();
-    typingTxt = typingTxt.split(""); // 한글자씩 자른다.
-    if (typingBool == false) {
-      // 타이핑이 진행되지 않았다면
-      typingBool = true;
-      var tyInt = setInterval(typing, 100); // 반복동작
-    }
- 
